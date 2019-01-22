abcb8da8926cfdec6864082694ddb623e2541e46
sWIFchbebs <christian@bebst.de>
2019-01-22 13:10:35 +0100

    Ihre Commit-Nachricht sWIFchbebs

--- src/main/java/Bug.java
+++ src/main/java/Bug.java
@@ -6,13 +6,19 @@ public class Bug {
 	 * @throws IllegalArgumentException if referenced array is null or empty
 	 */
 	public static int minimum(int[] a) throws IllegalArgumentException {
-		int m = a[0];
-		for (int i = 1; i < a.length; i++) {
-			if (a[i] > m) {
-				m = a[i];
+		if(a == null || a.length == 0){
+			throw new IllegalArgumentException("Array can't be null or empty");
+		} else {
+			int m = a[0];
+			if (a.length > 1) {
+				for (int i = 1; i < a.length; i++) {
+					if (a[i] < m) {
+						m = a[i];
+					}
+				}
 			}
-		}
 
-		return m;
+			return m;
+		}
 	}
 }
